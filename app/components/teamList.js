58e375a0438f65040542f943cd4052e50652e445
ilanshenar <ilan56@gmail.com>
2016-11-14 16:09:05 -0500

    added profile components

---
+++ app/components/teamList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+export default class TeamList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = props.data;
+  }
+
+  render() {
+    return (
+      <div className="col-md-2 right-sidebar">
+          <ul className="nav nav-pills nav-stacked">
+              <li role="presentation">Team Members</li>
+              {this.state.members.map((team) => {
+                return (
+                      <li role="presentation"><a href="#">{team.name}</a></li>
+                    )
+                  })
+                }
+          </ul>
+      </div>
+    )
+  }
+}
