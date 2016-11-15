817a81c085cc136ed11e156290e328c868fbbd29
klynnsmith <kimberlysmit@umass.edu>
2016-11-15 18:39:18 -0500

    updated profile component

--- app/components/profile.js
+++ app/components/profile.js
@@ -4,7 +4,6 @@ import PlayerSummary from './teamSummary';
 import TeamList from './teamList';
 import Schedule from './pageSchedule';
 
-
 export default class Profile extends React.Component {
   constructor(props) {
     super(props);
@@ -13,10 +12,19 @@ export default class Profile extends React.Component {
     };
   }
 
+  checkReferee(){
+    var refCheck = true;
+    if(this.state.referee === false){
+      refCheck = false;
+    }
+    return refCheck;
+  }
+
 render() {
+
+  if(this.checkReferee() === true){
   return (
-    <Schedule />
-    /*<div>
+    <div>
       <PlayerSummary data={this.state.summary}/>
       <PlayerDescription data={this.state}/>
       {this.state.contents.map((review) => {
@@ -25,8 +33,26 @@ render() {
         )
       })
     }
-    <TeamList data={this.state.list}/>
-    </div>*/
+    <Schedule data={this.state.schedule}/>
+  </div>
     )
   }
+
+  if(this.checkReferee() === false){
+  return (
+    <div>
+      <PlayerSummary data={this.state.summary}/>
+      <PlayerDescription data={this.state}/>
+      {this.state.contents.map((review) => {
+      return (
+          <Review key={review._id} data={review} />
+        )
+      })
+    }
+    <Schedule data={this.state.schedule}/>
+    <TeamList data={this.state.list}/>
+  </div>
+    )
+  }
+}
 }
