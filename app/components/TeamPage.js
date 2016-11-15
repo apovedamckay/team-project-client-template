bd2313754c4bb25da301568c657dea5a12100afc
Maxwell Galat <mgalat@umass.edu>
2016-11-15 15:24:15 -0500

    pulling

--- app/components/TeamPage.js
+++ app/components/TeamPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Review from './pageReviews';
 import TeamSummary from './teamSummary';
+import TeamDescription from './teamDescription';
 import Roster from './roster';
 import {getTeamData} from '../server';
 
@@ -8,7 +9,11 @@ import {getTeamData} from '../server';
 export default class TeamPage extends React.Component {
   constructor(props) {
     super(props);
-    //this.state = this.props;
+    this.state = this.props;
+  }
+
+  componentDidMount() {
+    this.refresh();
   }
 
   refresh() {
@@ -22,7 +27,7 @@ render() {
     <div>
       <TeamSummary data={this.state.summary}/>
       <TeamDescription data={this.state}/>
-      {this.state.contents.map((review) => {
+      {this.state.reviews.map((review) => {
       return (
           <Review key={review._id} data={review} />
         )
