c503cb15e4532361023c60b309a5715175894736
ilanshenar <ilan56@gmail.com>
2016-12-02 19:23:48 -0500

    working server

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,24 @@
-// Implement your server in this file.
-// We should be able to run your server with node src/server.js
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+// Defines what happens when it receives the `GET /` request
+app.use(express.static('../client/build'));
+
+/**
+* Translate JSON Schema Validation failures into error 400s.
+*/
+app.use(function(err, req, res, next) {
+if (err.name === 'JsonSchemaValidation') {
+// Set a bad request http response status
+res.status(400).end();
+} else {
+// It's some other sort of error; pass it to next error middleware handler
+next(err);
+}
+});
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!');
+});
