daf02b68a2a2c9a73bc95bca0ac19f452539bb73
ilanshenar <ilan56@gmail.com>
2016-12-12 18:46:26 -0500

    resetdatabase fixed

--- server/src/server.js
+++ server/src/server.js
@@ -237,14 +237,14 @@ next(err);
 });
 
 
-// Reset database.
+/// Reset the database.
 app.post('/resetdb', function(req, res) {
-console.log("Resetting database...");
-// This is a debug route, so don't do any validation.
-database.resetDatabase();
-// res.send() sends an empty response with status code 200
-res.send();
+  console.log("Resetting database...");
+  ResetDatabase(db, function() {
+    res.send();
+  });
 });
+
 
 
 // Starts the server on port 3000!
